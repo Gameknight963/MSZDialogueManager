3758eb8f13a55a55bbdbbb9c2bfe615a8aa9143f
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-03-05 01:10:58 -0600

    add more sidebar features

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,8 +34,12 @@
             this.textColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.statusLabel = new System.Windows.Forms.Label();
             this.jsonButton = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.templeteButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.nextNodesBox = new System.Windows.Forms.ListBox();
+            this.nextNodesHeader = new System.Windows.Forms.Label();
+            this.textHeaderLabel = new System.Windows.Forms.Label();
+            this.textLabel = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,6 +57,7 @@
             this.dialogueView.TabIndex = 0;
             this.dialogueView.UseCompatibleStateImageBehavior = false;
             this.dialogueView.View = System.Windows.Forms.View.Details;
+            this.dialogueView.SelectedIndexChanged += new System.EventHandler(this.dialogueView_SelectedIndexChanged);
             // 
             // indexColumn
             // 
@@ -74,7 +79,7 @@
             this.statusLabel.AutoSize = true;
             this.statusLabel.Location = new System.Drawing.Point(12, 428);
             this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(295, 13);
+            this.statusLabel.Size = new System.Drawing.Size(313, 13);
             this.statusLabel.TabIndex = 1;
             this.statusLabel.Text = "Click \"Load from JSON\" or \"Initialize Templete\" to get started";
             // 
@@ -88,23 +93,67 @@
             this.jsonButton.UseVisualStyleBackColor = true;
             this.jsonButton.Click += new System.EventHandler(this.jsonButton_Click);
             // 
-            // button2
+            // templeteButton
             // 
-            this.button2.Location = new System.Drawing.Point(3, 383);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(202, 23);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Initialize Templete";
-            this.button2.UseVisualStyleBackColor = true;
+            this.templeteButton.Location = new System.Drawing.Point(3, 383);
+            this.templeteButton.Name = "templeteButton";
+            this.templeteButton.Size = new System.Drawing.Size(202, 23);
+            this.templeteButton.TabIndex = 0;
+            this.templeteButton.Text = "Initialize Templete";
+            this.templeteButton.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.nextNodesBox);
+            this.panel1.Controls.Add(this.nextNodesHeader);
+            this.panel1.Controls.Add(this.textHeaderLabel);
+            this.panel1.Controls.Add(this.textLabel);
             this.panel1.Controls.Add(this.jsonButton);
-            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.templeteButton);
             this.panel1.Location = new System.Drawing.Point(580, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(208, 413);
             this.panel1.TabIndex = 2;
+            // 
+            // nextNodesBox
+            // 
+            this.nextNodesBox.FormattingEnabled = true;
+            this.nextNodesBox.Location = new System.Drawing.Point(6, 149);
+            this.nextNodesBox.Name = "nextNodesBox";
+            this.nextNodesBox.Size = new System.Drawing.Size(199, 56);
+            this.nextNodesBox.TabIndex = 3;
+            this.nextNodesBox.DoubleClick += new System.EventHandler(this.nextNodesBox_DoubleClick);
+            // 
+            // nextNodesHeader
+            // 
+            this.nextNodesHeader.AutoSize = true;
+            this.nextNodesHeader.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextNodesHeader.Location = new System.Drawing.Point(3, 130);
+            this.nextNodesHeader.Margin = new System.Windows.Forms.Padding(3);
+            this.nextNodesHeader.Name = "nextNodesHeader";
+            this.nextNodesHeader.Size = new System.Drawing.Size(70, 13);
+            this.nextNodesHeader.TabIndex = 2;
+            this.nextNodesHeader.Text = "Next nodes:";
+            // 
+            // textHeaderLabel
+            // 
+            this.textHeaderLabel.AutoSize = true;
+            this.textHeaderLabel.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textHeaderLabel.Location = new System.Drawing.Point(3, 6);
+            this.textHeaderLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.textHeaderLabel.Name = "textHeaderLabel";
+            this.textHeaderLabel.Size = new System.Drawing.Size(102, 13);
+            this.textHeaderLabel.TabIndex = 2;
+            this.textHeaderLabel.Text = "Selected item text:";
+            // 
+            // textLabel
+            // 
+            this.textLabel.Location = new System.Drawing.Point(3, 25);
+            this.textLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(202, 99);
+            this.textLabel.TabIndex = 1;
+            this.textLabel.Text = "Text will appear here";
             // 
             // Form1
             // 
@@ -114,9 +163,11 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.statusLabel);
             this.Controls.Add(this.dialogueView);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,8 +181,12 @@
         private System.Windows.Forms.ColumnHeader textColumn;
         private System.Windows.Forms.Label statusLabel;
         private System.Windows.Forms.Button jsonButton;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button templeteButton;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.Label textHeaderLabel;
+        private System.Windows.Forms.ListBox nextNodesBox;
+        private System.Windows.Forms.Label nextNodesHeader;
     }
 }
 
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSZDialougeManager
+{
+    public partial class Form1 : Form
+    {
+        public static List<DialogueNodeDTO> nodes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dialogueView.Columns[2].Width -= SystemInformation.VerticalScrollBarWidth;
+            SetSidebarMode(SidebarMode.Idle);
+        }
+
+        private enum SidebarMode
+        {
+            ItemSelected,
+            Idle,
+        }
+
+        private void SetSidebarMode(SidebarMode mode)
+        {
+            switch (mode)
+            {
+                case SidebarMode.Idle:
+                    jsonButton.Visible = true;
+                    templeteButton.Visible = true;
+                    textLabel.Visible = false;
+                    textHeaderLabel.Visible = false;
+                    nextNodesHeader.Visible = false;
+                    nextNodesBox.Visible = false;
+                    break;
+                case SidebarMode.ItemSelected:
+                    jsonButton.Visible = false;
+                    templeteButton.Visible = false;
+                    textLabel.Visible = true;
+                    textHeaderLabel.Visible = true;
+                    nextNodesHeader.Visible = true;
+                    nextNodesBox.Visible = true;
+                    nextNodesBox.UpdateNodesBox(GetSelectedNode().nextNodeIds);
+                    break;
+            }
+        }
+
+        private void jsonButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fd = new OpenFileDialog())
+            {
+                fd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                fd.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+                fd.Multiselect = false;
+
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    string json = File.ReadAllText(fd.FileName);
+                    nodes = JsonConvert.DeserializeObject<List<DialogueNodeDTO>>(json);
+                    dialogueView.UpdateDialogueView(nodes);
+                }
+            }
+        }
+
+        private DialogueNodeDTO GetSelectedNode()
+        {
+            int index = dialogueView.SelectedItems[0].Index;
+            return nodes[index];
+        }
+
+        private void dialogueView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (dialogueView.SelectedItems.Count == 0)
+            {
+                SetSidebarMode(SidebarMode.Idle);
+                return;
+            }
+            SetSidebarMode(SidebarMode.ItemSelected);
+
+            textLabel.Text = GetSelectedNode().dialogueText;
+        }
+
+        private void nextNodesBox_DoubleClick(object sender, EventArgs e)
+        {
+            int index = nextNodesBox.SelectedIndex;
+            if (index == -1) return;
+            NextNodesBoxItem item = (NextNodesBoxItem)nextNodesBox.SelectedItem;
+            dialogueView.SelectedItems.Clear();
+            dialogueView.Items[item.node.id].Selected = true;
+            nextNodesBox.UpdateNodesBox(GetSelectedNode().nextNodeIds);
+        }
+    }
+}
