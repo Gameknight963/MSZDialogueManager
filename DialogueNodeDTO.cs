abf67e224c9c65deec6e7f2ddb3e81de0bc7a81f
sebi <sebisebpaw@gmail.com>
2026-03-05 00:11:08 -0600

    add json loading functionality

---
+++ DialogueNodeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSZDialougeManager
+{
+    public class DialogueNodeDTO
+    {
+        public int id;
+        public int[] nextNodeIds;
+        public string dialogueText;
+        public string speakerName;
+        public float delay;
+    }
+}
---
+++ Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace MSZDialougeManager
+{
+    public static class Extensions
+    {
+        public static void UpdateDialogueView(this ListView dialogueView, List<DialogueNodeDTO> nodes)
+        {
+            dialogueView.Items.Clear();
+            foreach (DialogueNodeDTO n in nodes)
+            {
+                ListViewItem item = new ListViewItem(n.id.ToString());
+                item.SubItems.Add(n.speakerName);
+                item.SubItems.Add(n.dialogueText);
+                dialogueView.Items.Add(item);
+            }
+        }
+    }
+}
