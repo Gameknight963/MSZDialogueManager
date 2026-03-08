c584790fe07243a8769b6a631da4c63494f17ce5
sebi <sebisebpaw@gmail.com>
2026-03-07 22:51:31 -0600

    improve form1.cs code, move naudio functions to NAudioHelpers

---
+++ NAudioHelpers.cs
@@ -0,0 +1,39 @@
+﻿using NAudio.Vorbis;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSZDialougeManager
+{
+    public class NAudioHelpers
+    {
+        public static void PlayAudio(string file, ref IWavePlayer waveOut, ref WaveStream audioStream)
+        {
+            if (string.IsNullOrEmpty(file) || !File.Exists(file)) return;
+
+            StopAudio(ref waveOut, ref audioStream);
+
+            if (Path.GetExtension(file).ToLower() == ".ogg")
+                audioStream = new VorbisWaveReader(file);
+            else
+                audioStream = new AudioFileReader(file);
+
+            waveOut = new WaveOutEvent();
+            waveOut.Init(audioStream);
+            waveOut.Play();
+        }
+        public static void StopAudio(ref IWavePlayer waveOut, ref WaveStream audioStream)
+        {
+            waveOut?.Stop();
+            waveOut?.Dispose();
+            waveOut = null;
+
+            audioStream?.Dispose();
+            audioStream = null;
+        }
+    }
+}
